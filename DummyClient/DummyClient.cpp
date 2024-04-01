ef88c6221ae4b6c976e678b23380811cc0089b76
hans4809 <58892107+hans4809@users.noreply.github.com>
2024-04-01 19:46:38 +0900

    select 모델

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -25,61 +25,149 @@ int main()
 	// protocol : 0
 	//return : descriptor
 	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
-	if(clientSocket == INVALID_SOCKET)
+	if (clientSocket == INVALID_SOCKET)
 	{
 		int32 errCode = ::WSAGetLastError();
 		cout << "Socket ErrorCode : " << errCode << endl;
 		return 0;
 	}
+#pragma region TCP
+	//// 연결할 목적지는? (IP 주소 + Port)
+	//SOCKADDR_IN serverAddr; // IPv4
+	//::memset(&serverAddr, 0, sizeof(serverAddr));
+	//serverAddr.sin_family = AF_INET;
+	////serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1");
+	//::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	//serverAddr.sin_port = ::htons(7777);
 
-	// 연결할 목적지는? (IP 주소 + Port)
-	SOCKADDR_IN serverAddr; // IPv4
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	//serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1");
-	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-	serverAddr.sin_port = ::htons(7777);
+	//if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	//{
+	//	int32 errCode = ::WSAGetLastError();
+	//	cout << "Connect ErrorCode : " << errCode << endl;
+	//	return 0;
+	//}
 
-	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	//// 연결 성공 TODO : 데이터 송수신
+	//cout << "Connected To Server!" << endl;
+
+	//while (true)
+	//{
+	//	//TODO
+	//	char sendBuffer[100] = "Hello World!";
+	//	int resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
+	//	if (resultCode == SOCKET_ERROR)
+	//	{
+	//		int errCode = ::WSAGetLastError();
+	//		cout << "Send ErrorCode" << errCode << endl;
+	//		return 0;
+	//	}
+
+	//	cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
+
+	//	char recvBuffer[1000];
+
+	//	int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+
+	//	if (recvLen <= 0)
+	//	{
+	//		int errCode = ::WSAGetLastError();
+	//		cout << "Recv ErrorCode : " << errCode << endl;
+	//		return 0;
+	//	}
+
+	//	cout << "Recv Data! Data = " << recvBuffer << endl;
+	//	cout << "Recv Data! Len = " << recvLen << endl;
+
+	//	this_thread::sleep_for(1s);
+	//}
+#pragma endregion
+
+#pragma region Non-Blocking
+	u_long on = 1;
+	if (::ioctlsocket(clientSocket, FIONBIO, &on) == INVALID_SOCKET)
 	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "Connect ErrorCode : " << errCode << endl;
 		return 0;
 	}
 
-	// 연결 성공 TODO : 데이터 송수신
-	cout << "Connected To Server!" << endl;
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777);
 
+	//Connect
 	while (true)
 	{
-		//TODO
-		char sendBuffer[100] = "Hello World!";
-		int resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
-		if (resultCode == SOCKET_ERROR)
+		if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
 		{
-			int errCode = ::WSAGetLastError();
-			cout << "Send ErrorCode" << errCode << endl;
-			return 0;
+			// 논블로킹이라 바로 에러가 나지 않음
+			if (WSAGetLastError() == WSAEWOULDBLOCK)
+			{
+				continue;
+			}
+			// 이미 연결된 상태
+			if (WSAGetLastError() == WSAEISCONN)
+			{
+				cout << "Connected To Server!" << endl;
+				break;
+			}
+			// 그 외의 에러
+			break;
+		}
+	}
+
+	cout << "Connected To Server!" << endl;
+
+	char sendBuffer[100] = "Hello World!";
+	while (true)
+	{
+		if (::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0) == SOCKET_ERROR)
+		{
+			// 논블로킹으로 해놔서 다시 체크해야댐
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+			{
+				continue;
+			}
+
+			// 에러
+			break;
 		}
 
 		cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
-
-		char recvBuffer[1000];
-
-		int32 recvLen =  ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-
-		if (recvLen <= 0)
+		
+		// Recv
+		while (true)
 		{
-			int errCode = ::WSAGetLastError();
-			cout << "Recv ErrorCode : " << errCode << endl;
-			return 0;
+			char recvBuffer[1000];
+			int recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+
+			if (recvLen == SOCKET_ERROR)
+			{
+				// 논블로킹으로 해놔서 다시 체크해야댐
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				{
+					continue;
+				}
+
+				// 에러
+				break;
+			}
+			else if (recvLen == 0)
+			{
+				// 연결 끊김
+				break;
+			}
+
+			cout<< "Recv Data! Len = " << recvLen << endl;
+			break;
 		}
 
-		cout << "Recv Data! Data = " << recvBuffer << endl;
-		cout << "Recv Data! Len = " << recvLen << endl;
 
 		this_thread::sleep_for(1s);
 	}
+#pragma endregion
+
+	
 	
 	// 소켓 리소스 반환
 	::closesocket(clientSocket);
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -18,6 +18,14 @@ void HandleError(const char* cause)
 	cout << cause << "ErrorCode : " << errCode << endl;
 }
 
+const int BUFFER_SIZE = 1000;
+struct Session
+{
+	SOCKET socket;
+	char recvBuffer[BUFFER_SIZE] = {};
+	int32 recvBytes = 0;
+	int32 sendBytes = 0;
+};
 int main()
 {
 	WSAData wsaData;
@@ -101,6 +109,13 @@ int main()
 #pragma endregion
 
 #pragma region Non-Blocking
+	// 블로킹 소켓
+	// accept -> 접속한 클라가 있을 때
+	// connect -> 서버에 접속 성공했을 때
+	// send, sendto -> 요청한 데이터를 송신 버퍼에 복사했을 때
+	// recv, recvfrom -> 수신 버퍼에 데이터가 있고, 이를 유저 레벨 버퍼에서 복사했을 때
+	
+	// 논블로킹 소켓
 	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (listenSocket == INVALID_SOCKET)
 	{
@@ -131,17 +146,115 @@ int main()
 
 	cout << "Accept" << endl;
 
-	SOCKADDR_IN clientAddr;
-	int32 addrLen = sizeof(clientAddr);
+	// Select 모델 = (select 함수가 핵심이 됨)
+	// 소켓 함수 호출이 성공할 시점을 미리 알 수 잇다.
+	// 문제 상황)
+	// 수신 버퍼에 데이터가 없는데, read 한다거나
+	// 송신 버퍼가 꽉 차있는데 write 한다거나
+	// - 블로킹 소켓 : 조건이 만족되지 않아서 블로킹 되는 상황 예방
+	// - 논블로킹 소켓 : 조건이 만족되지 않아서 불필요하게 반복 체크하는 상황을 예방
+
+	// socket set
+	// 1) 읽기[ ] 쓰기[ ] 예외[ ] 관찰 대상 등록
+	// OutOfBand는 send() 마지막 인자 MSG_OOB로 보내는 특별한 데이터
+	// 받는 쪽에서도 recv OOB 세팅을 해야 읽을 수 있음
+	// 2) select(readSet, writeSet, exceptSet) : -> 관찰 시작
+	// 3) 적어도 하나의 소켓이 준비되면 리턴 -> 낙오자는 알아서 제거됨
+	// 4) 준비된 소켓을 찾아서 처리
+
+	// fd_set set;
+	// FD_ZERO : 비운다
+	// ex) FD_ZERO(&set);
+	// FD_SET : 소켓 s를 넣는다.
+	// ex) FD_SET(s, &set);
+	// FD_CLR : 소켓 s를 제거
+	// ex) FD_CLR(s, &set);
+	// FD_ISSET : 소켓 s가 set에 있으면 0이 아닌 값을 리턴
+
+	vector<Session> sessions;
+	sessions.reserve(100);
+
+	fd_set reads;
+	fd_set writes;
+
 	while (true)
 	{
-		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
-		if (clientSocket == INVALID_SOCKET)
+		// 소켓 set 초기화
+		FD_ZERO(&reads);
+		FD_ZERO(&writes);
+
+		// ListenSocket을 등록
+		FD_SET(listenSocket, &reads);
+
+		// 소켓 등록
+		for (Session& s : sessions)
 		{
-			//논블로킹으로 해놔서 다시 체크해야댐
-			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+			if(s.recvBytes <= s.sendBytes)
+				FD_SET(s.socket, &reads);
+			else
+				FD_SET(s.socket, &writes);
+		}
+
+		// [옵션] 마지막 timeout 인자 설정 가능
+		int32 retVal = ::select(0, &reads, &writes, nullptr, nullptr);
+		if (retVal == SOCKET_ERROR)
+		{
+			break;
+		}
+
+		// Listener 소켓 체크
+		if (FD_ISSET(listenSocket, &reads))
+		{
+			SOCKADDR_IN clientAddr;
+			int32 addrLen = sizeof(clientAddr);
+			SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		
+			if (clientSocket != INVALID_SOCKET)
 			{
-				continue;
+				cout << "Client Connected!" << endl;
+				sessions.push_back(Session{ clientSocket });
+			}
+		}
+		
+		// 나머지 소켓 체크
+		for (Session& s : sessions)
+		{
+			// Read
+			if (FD_ISSET(s.socket, &reads))
+			{
+				int32 recvLen = ::recv(s.socket, s.recvBuffer, BUFFER_SIZE, 0);
+				if (recvLen <= 0)
+				{
+					::closesocket(s.socket);
+					s = sessions.back();
+					sessions.pop_back();
+					continue;
+				}
+				else
+				{
+					s.recvBytes = recvLen;
+				}
+			}
+
+			// Write
+			if (FD_ISSET(s.socket, &writes))
+			{
+				// 블로킹 모드 -> 모든 데이터 다 보냄
+				// 논브로킹 모드 -> 일부만 보낼 수 있음 (상대방 수신 버퍼 상황에 따라)
+				int32 sendLen = ::send(s.socket, &s.recvBuffer[s.sendBytes], s.recvBytes, 0);
+				if (sendLen == SOCKET_ERROR)
+				{
+					::closesocket(s.socket);
+					s = sessions.back();
+					sessions.pop_back();
+					continue;
+				}
+				s.sendBytes += sendLen;
+				if (s.recvBytes == s.sendBytes)
+				{
+					s.recvBytes = 0;
+					s.sendBytes = 0;
+				}
 			}
 		}
 	}
