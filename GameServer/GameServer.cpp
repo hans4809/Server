234df3737ab0344da0aa00102c46cf9c7376d016
hans4809 <58892107+hans4809@users.noreply.github.com>
2024-06-19 23:57:04 +0900

    XML Parser

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -13,6 +13,7 @@
 #include "Player.h"
 #include "DBConnectionPool.h"
 #include "DBBind.h"
+#include "XMLParser.h"
 
 enum
 {
@@ -39,63 +40,6 @@ void DoWorkerJob(ServerServiceRef& service)
 int main()
 {
 	ASSERT_CRASH(GDBConnectionPool->Connect(1, L"Driver={ODBC Driver 17 for SQL Server};Server=(localdb)\\MSSQLLocalDB;Database=ServerDB;Trusted_Connection=Yes;"));
-
-	// CREATE TABLE
-	{
-		auto query = L"									\
-			DROP TABLE IF EXISTS [dbo].[Gold]			\
-			CREATE TABLE [dbo].[Gold]					\
-			(											\
-				[id] INT NOT NULL PRIMARY KEY IDENTITY,	\
-				[gold] INT NULL							\
-			);";
-
-		DBConnection* dbConnection = GDBConnectionPool->Pop();
-		ASSERT_CRASH(dbConnection->Execute(query));
-		GDBConnectionPool->Push(dbConnection);
-	}
-
-	//ADD DATA
-	for (int32 i = 0; i < 3; i++)
-	{
-		DBConnection* dbConnection = GDBConnectionPool->Pop();
-		dbConnection->Unbind();
-
-		int32 gold = 100;
-		SQLLEN len = 0;
-
-		ASSERT_CRASH(dbConnection->BindParam(1, &gold, &len));
-
-		ASSERT_CRASH(dbConnection->Execute(L"INSERT INTO [dbo].[Gold] ([gold]) VALUES (?)"));
-		GDBConnectionPool->Push(dbConnection);
-	}
-
-	//READ
-	{
-		DBConnection* dbConnection = GDBConnectionPool->Pop();
-		dbConnection->Unbind();
-
-		int32 gold = 100;
-		SQLLEN len = 0;
-		ASSERT_CRASH(dbConnection->BindParam(1, &gold, &len));
-
-		int32 outID = 0;
-		SQLLEN outIDLen = 0;
-		ASSERT_CRASH(dbConnection->BindColumn(1,&outID, &outIDLen));
-
-		int32 outGold = 0;
-		SQLLEN outGoldLen = 0;
-		ASSERT_CRASH(dbConnection->BindColumn(2, &outGold, &outGoldLen));
-
-		ASSERT_CRASH(dbConnection->Execute(L"SELECT * FROM [dbo].[Gold] WHERE gold = (?)"));
-	
-		while (dbConnection->Fetch())
-		{
-			cout << "ID : " << outID << " Gold : " << outGold << endl;
-		}
-		GDBConnectionPool->Push(dbConnection);
-	}
-
 
 	ClientPacketHandler::Init();
 
